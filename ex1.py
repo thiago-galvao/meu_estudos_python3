8420180ede06ac9d7c149994633b8d64040c9723
thiago-galvao <146104534+thiago-galvao@users.noreply.github.com>
2024-10-22 14:33:22 -0300

    Update and rename README.md to ex1.py
    Postagem do primeiro exercício que realizei com Python.

---
+++ ex1.py
@@ -0,0 +1,20 @@
+import random
+
+computador = random.randint(0, 10)
+print(
+        """Sou seu computador..
+Acabei de pensar em um número entre 0 e 10,
+Será que você consegue adivinhar em qual número eu pensei?"""
+        )
+acertou = False
+palpite = 0
+while not acertou:
+    jogador = int(input("Qual o seu palpite? "))
+    palpite += 1
+    if jogador < computador:
+        print("Mais... Tente novamente.")
+    if jogador > computador:
+        print("Menos... Tente novamente")
+    elif jogador == computador:
+        acertou = True
+print("Meus parabéns, você acertou na {} tentativa.".format(palpite))
